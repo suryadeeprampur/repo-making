c10f1d0b25ab045f567fa279db8b0448de963766
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-06-16 21:33:45 +0530

    Update config.py

---
+++ config.py
@@ -0,0 +1,117 @@
+import os, time, re
+id_pattern = re.compile(r'^.\d+$')
+
+
+
+class Config(object):
+    # pyro client config
+    API_ID    = os.environ.get("API_ID", "")
+    API_HASH  = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+   
+    # database config
+    DATABASE_NAME = os.environ.get("DATABASE_NAME","")     
+    DATABASE_URL  = os.environ.get("DATABASE_URL","")
+ 
+    # other configs
+    BOT_UPTIME  = time.time()
+    START_PIC   = os.environ.get("START_PIC", "")
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+
+    # channels logs
+    FORCE_SUBS   = os.environ.get("FORCE_SUBS", "") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+
+    # wes response configuration     
+    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
+
+
+
+class Txt(object):
+    # part of text configuration
+    START_TXT = """Hello {} 👋 
+
+➻ This Is An Advanced And Yet Powerful Rename Bot.
+
+➻ Using This Bot You Can Rename And Change Thumbnail Of Your Files.
+
+➻ You Can Also Convert Video To File And File To Video.
+
+➻ This Bot Also Supports Custom Thumbnail And Custom Caption.
+
+<b>Bot Is Made By :</b> @MadflixBotz"""
+
+    ABOUT_TXT = """
+╭───────────────⍟
+├<b>🤖 My Name</b> : {}
+├<b>🖥️ Developer</b> : <a href=https://t.me/RDX_PVT_LTD>RDX_PVT_LTD</a> 
+├<b>👨‍💻 Programer</b> : <a href=https://t.me/RDX1444>RDX Developer</a>
+├<b>📕 Library</b> : <a href=https://github.com/pyrogram>Pyrogram</a>
+├<b>✏️ Language</b> : <a href=https://www.python.org>Python 3</a>
+├<b>💾 Database</b> : <a href=https://cloud.mongodb.com>Mongo DB</a>
+├<b>📊 Build Version</b> : <a href=https://instagram.com/jishukumarsinha>Rename v4.7.0</a></b>     
+╰───────────────⍟
+"""
+
+    HELP_TXT = """
+🌌 <b><u>How To Set Thumbnail</u></b>
+  
+➪ /start - Start The Bot And Send Any Photo To Automatically Set Thumbnail.
+➪ /del_thumb - Use This Command To Delete Your Old Thumbnail.
+➪ /view_thumb - Use This Command To View Your Current Thumbnail.
+
+📑 <b><u>How To Set Custom Caption</u></b>
+
+➪ /set_caption - Use This Command To Set A Custom Caption
+➪ /see_caption - Use This Command To View Your Custom Caption
+➪ /del_caption - Use This Command To Delete Your Custom Caption
+➪ Example - <code>/set_caption 📕 Name ➠ : {filename}
+
+🔗 Size ➠ : {filesize} 
+
+⏰ Duration ➠ : {duration}</code>
+
+✏️ <b><u>How To Rename A File</u></b>
+
+➪ Send Any File And Type New File Name And Select The Format [ Document, Video, Audio ].           
+
+𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/RDX_PVT_LTD>Developer</a>
+"""
+
+    PROGRESS_BAR = """\n
+ <b>🔗 Size :</b> {1} | {2}
+️ <b>⏳️ Done :</b> {0}%
+ <b>🚀 Speed :</b> {3}/s
+️ <b>⏰️ ETA :</b> {4}
+"""
+
+    DONATE_TXT = """
+<b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
+
+If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
+
+<b>🛍 UPI ID:</b> `RDX_PVT_LTD`
+"""
+
+
+    SEND_METADATA = """<b><u>🖼️  HOW TO SET CUSTOM METADATA</u></b>
+
+For Example :-
+
+<code>By :- @RDX_PVT_LTD</code>
+
+💬 For Any Help Contact @RDX_PVT_LTD
+"""
+
+
+
+
+
+
+
+# RDX Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @RDX_PVT_LTD
+# Backup Channel @RDX_PVT_LTD
+# Developer @RDX_PVT_LTD
+# Contact @RDX1444
